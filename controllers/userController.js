ae6dc8182e999b074ad6f4c2e20ec5d78100980a
Alejandro García Serrano <agscm2@gmail.com>
2025-05-13 17:36:34 +0200

    Upload modified userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,19 @@
 const users = require("../db/users"); //MOCK
 const userModel = require("../models/userModel"); // Usando ya BBDD MongoDb
 
-const getAllUser = (req, res) => {
-  const users = userModel.find({name: "pepe"},{});
-  res.send(users);
+const getAllUser = async (req, res) => {
+  try {
+    const users = await userModel.find({},{});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({status: "Failed", error: error.message})
+  }
 };
 
 // Usamos find cuando el resultado solo es unico
-// Usamos flter si queremos que peuda devolver varios resultados
+// Usamos flter si queremos que pueda devolver varios resultados
 
 const getUserById = (req, res) => {
-  //const idUser = req.params.idUser;
   const { idUser } = req.params;
   console.log(idUser);
   const user = users.find((u) => u.id === parseInt(idUser));
@@ -18,12 +21,13 @@ const getUserById = (req, res) => {
   res.send(user);
 };
 
-const getUserByName = (req, res) => {
-  const { name } = req.params;
-  console.log(name);
-  const user = users.filter((u) => u.nombre.includes(name));
-  if (user.length === 0) return res.status(200).send("No hay usuarios con ese nombre");
-  res.send(user);
+const getUserByName = async (req, res) => {
+  try {
+    const users = await userModel.find({name: "Ana"},{});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({status: "Failed", error: error.message})
+  }
 };
 
 const getUserByEdad = (req, res) => {
@@ -35,8 +39,6 @@ const getUserByEdad = (req, res) => {
 };
 
 const addUser = async (req, res) => {
-  //console.log(newUser);
-    //res.send('Usuario creado');
   try {
     const newUser = req.body;
     await userModel.create(newUser);
