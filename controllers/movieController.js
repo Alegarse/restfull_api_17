b2ed72deb58ab58ab5401381a62848099d780329
Alejandro García Serrano <agscm2@gmail.com>
2025-05-14 20:28:23 +0200

    adding more methods to movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -22,7 +22,52 @@ const getAllMovies = async (req, res) => {
   }
 };
 
+const getMovieById = async (req, res) => {
+  try {
+    const { idMovie } = req.params;
+    const movie = await movieModel.findById(idMovie);
+    if (!movie) {
+      return res.status(200).send("No hay película por ese Id");
+    }
+    res.status(200).send({ status: "Success", data: movie });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
+const removeMovieById = async (req, res) => {
+  try {
+    const { idMovie } = req.params;
+    const movie = await movieModel.findByIdAndDelete(idMovie);
+    if (!movie) {
+      return res.status(200).send("No hay película por ese Id");
+    }
+    res
+      .status(200)
+      .send({ status: "Success", message: "La pelicula ha sido eliminada" });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
+const updateMovieById = async (req, res) => {
+  try {
+    const { idMovie } = req.params;
+    const newMovie = req.body;
+    const movie = await movieModel.findByIdAndUpdate(idMovie, newMovie);
+    if (!movie) {
+      return res.status(200).send("No hay película por ese Id");
+    }
+    res.status(200).send({ status: "Success", data: movie });
+  } catch (error) {
+    res.status(500).send({ status: "Failed", error: error.message });
+  }
+};
+
 module.exports = {
   addMovie,
   getAllMovies,
+  getMovieById,
+  removeMovieById,
+  updateMovieById,
 };
--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const {
   addMovie,
   getAllMovies,
+  getMovieById,
+  removeMovieById,
+  updateMovieById
 } = require("../controllers/movieController");
 
 router.get("/", getAllMovies);
+router.get("/:idMovie",getMovieById);
 router.post("/", addMovie);
+router.delete("/:idMovie",removeMovieById);
+router.patch("/",updateMovieById);
 
 // Ahora exportamos este router creado
 module.exports = router;
