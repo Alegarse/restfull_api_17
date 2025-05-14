2dda6983998ead86eee88876f74d72e8385d932c
Alejandro García Serrano <agscm2@gmail.com>
2025-05-14 19:37:14 +0200

    Signup finished

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,21 @@
 const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 
 const signup = async (req, res) => {
-
+    try {
+        const { name, lastName, email, password } = req.body;
+        const newUser = {
+            name,
+            lastName,
+            email,
+            password: await bcrypt.hash(password, 10)
+        }
+        await userModel.create(newUser);
+        res.status(200).send("El usuario se ha creado correctamente");
+    } catch (error) {
+      res.status(500).send({ status: "Failed", error: error.message });
+    }
 }
 
 module.exports = {
