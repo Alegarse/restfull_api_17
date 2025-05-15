237915ff5d12251ba1e0051507b1ccaa26e85390
Alejandro García Serrano <agscm2@gmail.com>
2025-05-15 18:23:35 +0200

    Secret y env

--- secret.js
+++ secret.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
  
 const secret = 'Full stack 17';
-const secret2 = 'Actualizo con más seguridad';
+const secret2 = 'Actualizo con más seguridad asegurada TOKEN DE REFRESCO';
  
 const hash = crypto.createHmac('sha256',secret).update(secret2).digest("hex");
  
