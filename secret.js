cdc775e3733c3a45af9663e01d7f6b779edf617c
AlexGS <44372747+Alegarse@users.noreply.github.com>
2025-05-15 23:22:52 +0200

    Delete secret.js

--- secret.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
- 
-const secret = 'Full stack 17';
-const secret2 = 'Actualizo con más seguridad asegurada TOKEN DE REFRESCO';
- 
-const hash = crypto.createHmac('sha256',secret).update(secret2).digest("hex");
- 
-console.log(hash);
